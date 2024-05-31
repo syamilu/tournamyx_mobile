7e6a808edc80e05a84fc25db59c0a3a271c2c63a
syamilu <wanmuhammadsyamil@gmail.com>
2024-05-31 20:04:55 +0800

    :sparkle: initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:tournamyx_mobile/utils/theme/color_schemes.g.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  runApp(const MyApp());
+
+  // runApp(
+  //   const ProviderScope(
+  //     child: MyApp(),
+  //   ),
+  // );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+final navigatorKey = GlobalKey<NavigatorState>();
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      debugShowCheckedModeBanner: false,
+      title: 'Tournamyx',
+      theme: ThemeData(
+        useMaterial3: true,
+        fontFamily: 'Poppins',
+        colorScheme: lightColorScheme,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(),
+      // routes: {
+      //   '/welcome': (context) => const WelcomeScreen(),
+      //   '/register': (context) => const RegisterScreen(),
+      //   '/login': (context) => const LoginScreen(),
+      // },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+//this is initial only
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tournamyx'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/welcome');
+              },
+              child: const Text('Welcome'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/register');
+              },
+              child: const Text('Register'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: const Text('Login'),
+            ),
+          ],
         ),
       ),
     );
