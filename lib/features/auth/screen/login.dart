10f45f488506ab0ec08a5539a110a32af2263bd0
syamilu <wanmuhammadsyamil@gmail.com>
2024-06-06 04:43:36 +0800

    :sparkle: login page and favourite page

--- lib/features/auth/screen/login.dart
+++ lib/features/auth/screen/login.dart
@@ -1,37 +1,221 @@
+//package imports
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+//local imports
+import 'package:tournamyx_mobile/components/auth/button.dart';
+import 'package:tournamyx_mobile/components/auth/text_field.dart';
+import 'package:tournamyx_mobile/utils/theme/tournamyx_theme.dart';
 
 class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+
+  final passwordController = TextEditingController();
+  final emailController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  void signUserIn() {
+    //TODO: implement auth
+    if (_formKey.currentState!.validate()) {
+      print('User signed in');
+    } else {
+      print('User not signed in');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
+      backgroundColor: TournamyxTheme.background,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              ColorFiltered(
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.7), // Adjust the opacity as needed
+                  BlendMode.darken,
+                ),
+                child: Image.network(
+                  'https://images.unsplash.com/photo-1593376853899-fbb47a057fa0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHJvYm90aWNzfGVufDB8fDB8fHww',
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: Implement login logic
-              },
-              child: Text('Login'),
-            ),
-          ],
+              Column(
+                children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.07),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                      padding: const EdgeInsets.only(left: 25.0),
+                      icon: const Icon(Icons.arrow_back_ios),
+                      color: TournamyxTheme.primary,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+                  const Text(
+                    "Welcome back to Tournamyx!",
+                    style: TextStyle(
+                      color: TournamyxTheme.primary,
+                      fontSize: 30, // Adjust the font size as needed
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    height: MediaQuery.of(context).size.height * 0.45,
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20, // Adjust the font size as needed
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                          ),
+                          SizedBox(
+                              height:
+                                  MediaQuery.of(context).size.height * 0.02),
+                          MyTextField(
+                            controller: emailController,
+                            hintText: 'Email',
+                            obscureText: false,
+                          ),
+                          SizedBox(
+                              height:
+                                  MediaQuery.of(context).size.height * 0.03),
+                          MyTextField(
+                            controller: passwordController,
+                            hintText: 'Password',
+                            obscureText: true,
+                          ),
+                          SizedBox(
+                              height:
+                                  MediaQuery.of(context).size.height * 0.03),
+                          MyButtonLogin(
+                            text: "Login",
+                            onTap: () {
+                              // Navigator.push(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //         builder: (context) => LoginPage())
+                              //         );
+                            },
+                          ),
+                          const SizedBox(height: 20),
+                          const Text('Forgot Password?',
+                              //TODO: implement forgot password
+                              style: TextStyle(
+                                  color: TournamyxTheme.primary,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14),
+                              textAlign: TextAlign.start),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                  Column(
+                    children: [
+                      const Text('Or login with',
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                      const SizedBox(height: 10),
+                      Row(
+                        // Login using other provider
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: const EdgeInsets.all(20),
+                            child: Icon(Ionicons.logo_google,
+                                color: Colors.yellow[700]),
+                          ),
+                          const SizedBox(width: 10),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: const EdgeInsets.all(20),
+                            child: const Icon(Ionicons.logo_facebook,
+                                color: Colors.blue),
+                          ),
+                          const SizedBox(width: 10),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: const EdgeInsets.all(20),
+                            child: const Icon(Ionicons.logo_apple,
+                                color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Don't have an account?",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14, // Adjust the font size as needed
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            // Navigator.push(
+                            //     context,
+                            //     MaterialPageRoute(
+                            //         builder: (context) => RegisterPage())
+                            //         );
+                          },
+                          child: const Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              color: TournamyxTheme.primary,
+                              fontSize: 14, // Adjust the font size as needed
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
