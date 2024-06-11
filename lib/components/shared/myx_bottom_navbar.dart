6124cd29a173cec2cb67e4348b1c9c8316669ab0
fertlas <fertlasqwerty@gmail.com>
2024-06-11 16:36:26 +0800

    "Added TourPage import and route,"

--- lib/components/shared/myx_bottom_navbar.dart
+++ lib/components/shared/myx_bottom_navbar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:flutter/rendering.dart';
 
 import 'package:tournamyx_mobile/features/auth/screen/login.dart';
 import 'package:tournamyx_mobile/features/favourite/screen/favourite_page.dart';
+import 'package:tournamyx_mobile/features/tour/page/tour_page.dart';
 import 'package:tournamyx_mobile/utils/theme/tournamyx_theme.dart';
 
 //type or model for bottombar
@@ -91,3 +93,60 @@ class _MyxBottomNavbarState extends State<MyxBottomNavbar> {
     );
   }
 }
+
+
+// //* My version of Bottom Nav Bar
+
+// class BottomAppBar extends StatefulWidget{
+//   const BottomAppBar({super.key});
+
+//   @override
+//   State createState() => _BottomAppBarState();
+// }
+
+// class _BottomAppBarState extends State <BottomAppBar> {
+//     int _selectedIndex = 0;
+//     bool _isVisible = true;
+//     ScrollController _scrollController = ScrollController();
+
+//     @override
+//     void initState() {
+//       super.initState();
+//       _scrollController.addListener((){
+//         if(_scrollController.position.userScrollDirection == ScrollDirection.reverse) {
+//           if (_isVisible == true) {
+//             setState(() {
+//               _isVisible = false;
+//             });
+//           }
+//         } else {
+//           if (_scrollController.position.userScrollDirection == ScrollDirection.forward) {
+//             if (_isVisible == false) {
+//               setState(() {
+//                 _isVisible = true;
+//               });
+//             }
+//           }
+//         }
+//       });
+//     }
+    
+//     @override
+//     void dispose() {
+//       _scrollController.dispose();
+//       super.dispose();
+//     }
+
+//     void _onItemTapped(int index) {
+//       setState(() {
+//         _selectedIndex = index;
+//       });
+//     }
+
+//     final List<Widget> _pages = [
+//       HomePage(),
+//       TourScreen(),
+//       FavouriteScreen(),
+//       SettingsPage(),
+//     ]
+// }
