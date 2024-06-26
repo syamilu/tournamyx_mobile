1a83346813c8824809e982a28d378389a42dca7f
fertlas <fertlasqwerty@gmail.com>
2024-06-26 23:52:15 +0800

    can see the table

--- lib/features/tour/model/team.dart
+++ lib/features/tour/model/team.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 class TeamModel {
   final String id;
   final String name;
---
+++ lib/features/tour/widgets/favbutton.dart
@@ -0,0 +1,8 @@
+// import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+// class FavButton extends StatefulWidget{
+//   final String 
+// }
+
